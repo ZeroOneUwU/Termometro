e56864cb83fb19feb10a9342137fac75aad734c7
eryka <eryka@LAPTOP-9IBP64BJ>
2022-05-30 19:44:06 -0500

    Agregar archivos de proyecto.

---
+++ Termometro/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Termometro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BotonSalir = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BotonSalir
+            // 
+            this.BotonSalir.Location = new System.Drawing.Point(548, 465);
+            this.BotonSalir.Name = "BotonSalir";
+            this.BotonSalir.Size = new System.Drawing.Size(75, 23);
+            this.BotonSalir.TabIndex = 0;
+            this.BotonSalir.Text = "Salir";
+            this.BotonSalir.UseVisualStyleBackColor = true;
+            this.BotonSalir.Click += new System.EventHandler(this.BotonSalir_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(614, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            40,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            40,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(22, 38);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(559, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 38);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(543, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "°";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Termometro.Properties.Resources.grados;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(648, 502);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.BotonSalir);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BotonSalir;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Termometro/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Termometro
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+           
+            //Declarar plumas de dibujo
+            Pen plumaRoja = new Pen(Color.Red, 3f);
+            this.Refresh();
+            //Declara la superficie de trabajo
+            Graphics superfice = CreateGraphics();
+
+            //Declarar los puntos o coordenadas
+            float x, y;
+
+            //PointF punto1 = new PointF(243,205);
+            //PointF punto2 = new PointF(243,60);
+            double angulo;
+            int magnitud = 180;
+            float cent = (int)numericUpDown1.Value * 3;
+            int X, Y;
+
+            //Dibuja la línea
+            /*
+            x = (float)numericUpDown1.Value+x;
+            y = (float)Math.Sin((float)x * (Math.PI / 180)) * 10;
+            punto2.X = x;
+            punto2.Y = y + 60;
+            */
+            angulo = cent;
+            angulo = (angulo - 90);
+            angulo = (angulo * Math.PI) / 180;
+
+            x = magnitud * (float)Math.Cos(angulo);
+            y = magnitud * (float)Math.Sin(angulo);
+
+            X = Convert.ToInt32(x);
+            Y = Convert.ToInt32(y);
+
+            
+            superfice.DrawLine(plumaRoja, 245, 205, (X + 243), (Y + 200));
+
+            //Libera el espacio
+            superfice.Dispose();
+            label1.Text = (numericUpDown1.Value + 20).ToString();
+        
+         
+          }
+
+        private void BotonSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+   
+        }
+
+    }
+}
